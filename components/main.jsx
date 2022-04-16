55891ca7d135795d375219d95a0d45f868157eff
ezraguy <55058653+ezraguy@users.noreply.github.com>
2022-04-16 21:46:44 +0300

    adding words checking feature

--- components/main.jsx
+++ components/main.jsx
@@ -2,9 +2,9 @@ import React, { useState, useId, useEffect } from "react";
 import styles from "../scss/Main.module.scss";
 const Main = () => {
   const [words, setWords] = useState([
-    { value: "dog", style: "curr" },
-    { value: "cat", style: "" },
-    { value: "bird", style: "" },
+    { value: "dog", style: "", isCurrent: true },
+    { value: "cat", style: "", isCurrent: false },
+    { value: "bird", style: "", isCurrent: false },
   ]);
   const [userInput, setUserInput] = useState("");
   const [currIndex, setCurrIndex] = useState(0);
@@ -17,26 +17,39 @@ const Main = () => {
   };
   const checkForMatch = () => {
     console.log(`word to check ${words[currIndex].value}`);
-    setCurrIndex(currIndex + 1);
-  };
+    let tempWords = [...words];
+    let wordToCheck = tempWords[currIndex];
+    // Compering the input with the word itself
+    wordToCheck.value === userInput
+      ? (tempWords[currIndex].style = "correct")
+      : (tempWords[currIndex].style = "wrong");
 
+    console.log(wordToCheck, userInput);
+    clearUserInput();
+    setWords(tempWords);
+    setCurrIndex(currIndex + 1);
+    changeCurrentWord();
+  };
+  const clearUserInput = () => {
+    setUserInput("");
+  };
   const changeCurrentWord = () => {
     const wordsTemp = [...words];
-    wordsTemp.map((word) => (word.style = ""));
-    wordsTemp[currIndex].style = "curr";
+    wordsTemp[currIndex].isCurrent = false;
+    wordsTemp[currIndex + 1].isCurrent = true;
     setWords(wordsTemp);
   };
 
-  useEffect(() => {
-    changeCurrentWord();
-  }, [currIndex]);
   return (
     <div className={styles.main}>
       <div className={styles.wordsWrap}>
         {words.map((word) => (
           <span
             key={`${id}-${word.value}`}
-            className={`${styles.word} ${word.style ? styles.curr : ""}`}
+            className={`${styles.word} ${word.isCurrent ? styles.current : ""} ${
+              word.style === "wrong" && styles.wrong
+            }
+            ${word.style === "correct" && styles.correct}`}
           >
             {word.value}
           </span>
